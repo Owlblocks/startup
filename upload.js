d0483f08e6643297eaa187fb40d52f838016a8a2
= <d-weatherford@live.com>
2023-03-25 00:48:13 -0600

    Upload button enabled upon finished preview.

--- upload.js
+++ upload.js
@@ -1,11 +1,12 @@
 const preview = document.getElementById('preview');
+const button = document.getElementById('upload')
 
 function onFileChanged(event) {
     preview.alt = 'Loading';
     preview.src = '';
     let file = event.srcElement.files[0]; 
     let fr = new FileReader();
-    fr.onload = function(event) { preview.src = event.target.result };
+    fr.onload = function(event) { preview.src = event.target.result; button.disabled = false; };
     fr.readAsDataURL(file);
 }
 
